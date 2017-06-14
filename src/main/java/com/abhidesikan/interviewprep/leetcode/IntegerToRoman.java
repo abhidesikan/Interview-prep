f9ccd8ad9cae59d8a14d6acf8939ace8eda78e19
Abhishek Desikan <abhidesikan@ambp.attlocal.net>
2017-06-13 22:09:32 -0400

    Integer to Roman

---
+++ src/main/java/com/abhidesikan/interviewprep/leetcode/IntegerToRoman.java
@@ -0,0 +1,21 @@
+package com.abhidesikan.interviewprep.leetcode;
+
+/**
+ * Created by abhidesikan on 6/13/17.
+ */
+public class IntegerToRoman {
+
+	public static String intToRoman(int num) {
+		String M[] = {"", "M", "MM", "MMM"};
+		String C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+		String X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+		String I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+		return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
+	}
+
+
+	public static void main(String[] args) {
+		System.out.println(intToRoman(3999));
+	}
+
+}
