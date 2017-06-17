d1b7cc16a0ed6cfd877a7a7b752a34d88369cfa9
Abhishek Desikan <abhidesikan@ambp.attlocal.net>
2017-06-17 15:58:05 -0400

    Indent

--- src/main/java/com/abhidesikan/interviewprep/leetcode/LongestCommonPrefix.java
+++ src/main/java/com/abhidesikan/interviewprep/leetcode/LongestCommonPrefix.java
@@ -9,27 +9,27 @@ public class LongestCommonPrefix {
 
 	public static String longestCommonPrefix(String[] strs) {
 
-		if(strs == null) {
+		if (strs == null) {
 			return null;
 		}
-		if(strs.length == 0) {
+		if (strs.length == 0) {
 			return "";
 		}
 
 		Arrays.sort(strs);
 
-		char first [] = strs[0].toCharArray();
-		char last [] = strs[strs.length -1].toCharArray();
+		char first[] = strs[0].toCharArray();
+		char last[] = strs[strs.length - 1].toCharArray();
 
 
-		int i=0, len = Math.min(first.length, last.length);
+		int i = 0, len = Math.min(first.length, last.length);
 
 		System.out.println(len);
-		while(i < len && first[i] == last[i]) {
+		while (i < len && first[i] == last[i]) {
 			System.out.println(i);
 			i++;
 		}
-		return strs[0].substring(0,i);
+		return strs[0].substring(0, i);
 	}
 
 	public static void main(String[] args) {
