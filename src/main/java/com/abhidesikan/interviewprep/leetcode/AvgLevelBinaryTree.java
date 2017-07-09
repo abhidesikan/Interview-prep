555ffc7e6b4b937c58572dd67692233233dadf37
Abhishek Desikan <abhidesikan@AMBP.local>
2017-07-08 23:14:13 -0400

    Average value of levels in a tree

---
+++ src/main/java/com/abhidesikan/interviewprep/leetcode/AvgLevelBinaryTree.java
@@ -0,0 +1,47 @@
+package com.abhidesikan.interviewprep.leetcode;
+
+import java.util.*;
+
+/**
+ * Created by abhidesikan on 7/8/17.
+ */
+public class AvgLevelBinaryTree {
+
+
+	public static List<Double> averageOfLevels(TreeNode node) {
+
+		List<Double> result = new ArrayList<Double>();
+		Queue<TreeNode> queue = new LinkedList<TreeNode>();
+
+		queue.offer(node);
+
+		while (!queue.isEmpty()) {
+			int n = queue.size();
+			double sum = 0;
+			for (int i = 0; i < n; i++) {
+				TreeNode temp = queue.poll();
+				sum += temp.val;
+				if (temp.left != null) {
+					queue.offer(temp.left);
+				}
+				if (temp.right != null) {
+					queue.offer(temp.right);
+				}
+			}
+			result.add(sum / n);
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		TreeNode node = new TreeNode(3);
+		node.left = new TreeNode(1);
+		node.right = new TreeNode(2);
+		node.left.left = new TreeNode(10);
+		node.left.right = new TreeNode(4);
+//		node.right.left = new TreeNode(4);
+//		node.right.left = new TreeNode(6);
+
+		System.out.println(averageOfLevels(node));
+	}
+}
