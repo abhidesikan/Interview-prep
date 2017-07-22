e10bb609a85a1e7f1cd1210cd1a2657aa8ade798
Abhishek Desikan <abhidesikan@AMBP.local>
2017-07-21 20:38:37 -0400

    Sub array sum

---
+++ src/main/java/com/abhidesikan/interviewprep/leetcode/SubArraySum.java
@@ -0,0 +1,30 @@
+package com.abhidesikan.interviewprep.leetcode;
+
+import java.util.Arrays;
+
+public class SubArraySum {
+
+	public int subarraySum(int[] nums, int k) {
+
+		int count = 0;
+		for(int i=0; i<nums.length; i++) {
+			int sum = nums[i];
+			if(sum == k) {
+				count++;
+			}
+			for(int j=i+1; j<nums.length; j++) {
+				sum += nums[j];
+				if(sum == k) {
+					count++;
+				}
+			}
+		}
+
+		return count;
+	}
+
+	public static void main(String[] args) {
+		int arr[] = {-1,-1,1};
+		System.out.println(new SubArraySum().subarraySum(arr, 0));
+	}
+}
