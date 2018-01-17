8f37c8854cc33f2a3badc52ff1caa2238dfaaf40
Abhishek Desikan <abhi.desikan@gmail.com>
2018-01-16 22:12:13 -0500

    Reverse linked list

---
+++ src/main/java/com/abhidesikan/interviewprep/leetcode/ReverseLinkedList.java
@@ -0,0 +1,17 @@
+package com.abhidesikan.interviewprep.leetcode;
+
+public class ReverseLinkedList {
+	public ListNode reverseList(ListNode head) {
+
+		ListNode prev = null;
+		ListNode next = null;
+		ListNode cur = head;
+		while(cur != null) {
+			next = cur.next;
+			cur.next = prev;
+			prev = cur;
+			cur = next;
+		}
+		return prev;
+	}
+}
