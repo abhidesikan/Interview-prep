add73fba1ee0644436bb817f5e9900cbce65fc66
Abhishek Desikan <abhi.desikan@gmail.com>
2018-01-30 19:29:35 -0500

    Two sum II

---
+++ src/main/java/com/abhidesikan/interviewprep/leetcode/TwoSumII.java
@@ -0,0 +1,29 @@
+package com.abhidesikan.interviewprep.leetcode;
+
+import java.util.Arrays;
+
+public class TwoSumII {
+
+	public static int[] twoSum(int[] numbers, int target) {
+		int start = 0;
+		int end = numbers.length - 1;
+
+		while (start < end) {
+			int sum = numbers[start] + numbers[end];
+			if (sum == target) {
+				return new int[]{start + 1, end + 1};
+			}
+			if (sum > target) {
+				end--;
+			} else {
+				start++;
+			}
+		}
+
+		return new int[]{};
+	}
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(twoSum(new int[]{1,2,5,3}, 8)));
+	}
+}
