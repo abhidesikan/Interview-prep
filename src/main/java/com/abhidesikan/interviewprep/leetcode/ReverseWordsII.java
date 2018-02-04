3ed075648b834e7fc002a42d8b589f98ae671939
Abhishek Desikan <abhi.desikan@gmail.com>
2018-02-03 19:56:17 -0500

    Reverse words II

---
+++ src/main/java/com/abhidesikan/interviewprep/leetcode/ReverseWordsII.java
@@ -0,0 +1,36 @@
+package com.abhidesikan.interviewprep.leetcode;
+
+import java.util.Arrays;
+
+public class ReverseWordsII {
+
+	public static void reverseWords(char[] str) {
+
+		swapChar(str, 0, str.length - 1);
+		int start = 0;
+		for (int i = 0; i < str.length; i++) {
+			if (str[i] == ' ') {
+				swapChar(str, start, i - 1);
+				start = i + 1;
+			}
+		}
+		swapChar(str, start, str.length - 1);
+	}
+
+	public static void swapChar(char[] str, int start, int end) {
+		while (start < end) {
+			char temp = str[start];
+			str[start] = str[end];
+			str[end] = temp;
+			start++;
+			end--;
+		}
+	}
+
+	public static void main(String[] args) {
+		char[] arr = new char[] {'a', 'b', 'h', 'i'};
+		reverseWords(arr);
+		System.out.println(Arrays.toString(arr));
+	}
+
+}
