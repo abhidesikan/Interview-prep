9c58a3335df28da17f1f8a5ce9bbc44c97aabb2b
Abhishek Desikan <abhi.desikan@gmail.com>
2018-02-08 20:03:46 -0500

    Count primes

---
+++ src/main/java/com/abhidesikan/interviewprep/leetcode/CountPrimes.java
@@ -0,0 +1,25 @@
+package com.abhidesikan.interviewprep.leetcode;
+
+public class CountPrimes {
+
+	public static int countPrimes(int n) {
+		int count = 0;
+
+		boolean notPrime[] = new boolean[n];
+
+		for (int i = 2; i < n; i++) {
+			if (notPrime[i] == false) {
+				count++;
+			}
+			for (int j = 2; i * j < n; j++) {
+				notPrime[i * j] = true;
+			}
+		}
+
+		return count;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(countPrimes(5));
+	}
+}
