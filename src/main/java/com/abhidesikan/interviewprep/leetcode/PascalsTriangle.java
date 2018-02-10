9f7bd7f0820e99a50aa248c5f1360db215511867
Abhishek Desikan <abhi.desikan@gmail.com>
2018-02-10 10:34:50 -0500

    Pascal's triangle

---
+++ src/main/java/com/abhidesikan/interviewprep/leetcode/PascalsTriangle.java
@@ -0,0 +1,28 @@
+package com.abhidesikan.interviewprep.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalsTriangle {
+
+	public static List<Integer> getRow(int rowIndex) {
+		List<Integer> list = new ArrayList();
+
+		if(rowIndex < 0) {
+			return list;
+		}
+
+		for(int i=0;i<=rowIndex;i++){
+			list.add(0,1);
+			for(int j=1; j<list.size()-1;j++){
+				list.set(j, list.get(j+1) + list.get(j));
+			}
+
+		}
+		return list;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(getRow(4));
+	}
+}
