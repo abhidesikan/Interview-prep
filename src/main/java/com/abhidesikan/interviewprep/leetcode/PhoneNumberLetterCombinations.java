a344bdb203dc40354a76ac808c06b7006e9d2a39
Abhishek Desikan <abhi.desikan@gmail.com>
2018-05-03 18:51:57 -0400

    Updated letter combinations

--- src/main/java/com/abhidesikan/interviewprep/leetcode/PhoneNumberLetterCombinations.java
+++ src/main/java/com/abhidesikan/interviewprep/leetcode/PhoneNumberLetterCombinations.java
@@ -1,32 +1,42 @@
 package com.abhidesikan.interviewprep.leetcode;
 
 import java.util.LinkedList;
-import java.util.List;
 
 public class PhoneNumberLetterCombinations {
 
-	public static List<String> letterCombinations(String digits) {
-		String arr[] = new String[]{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+	public static LinkedList<String> letterCombinations(String digits) {
 
-		LinkedList<String> list = new LinkedList<>();
+		String arr[] = new String[] {"+", "/", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 
-		if (digits.length() <= 0) {
-			return list;
-		}
+		LinkedList <String> list = new LinkedList<>();
+		list.add("");
 
-		String temp = "";
-		list.add(temp);
-
-		for (int i = 0; i < digits.length(); i++) {
-			int a = Character.getNumericValue(digits.charAt(i));
-			while (list.peek().length() == i) {
-				String t = list.remove();
-				for (Character c : arr[a].toCharArray()) {
-					list.add(t + c);
+		for(int i=0; i<digits.length(); i++) {
+			int curDigit = Character.getNumericValue(digits.charAt(i));
+			while(list.peek().length() == i) {
+				String temp = list.remove();
+				for(Character c : arr[curDigit].toCharArray()) {
+					list.add(temp + c);
 				}
 			}
 		}
+
+		list = removeDummy(list);
+
 		return list;
+
+	}
+
+
+	public static LinkedList<String> removeDummy(LinkedList<String> list) {
+
+		LinkedList<String>tempList = new LinkedList<String>();
+		for(String s : list) {
+			String temp = s.replaceAll("/", "");
+			tempList.add(temp);
+		}
+
+		return tempList;
 	}
 
 	public static void main(String[] args) {
