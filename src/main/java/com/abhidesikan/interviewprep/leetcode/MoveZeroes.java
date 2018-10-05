2257fae0cacd9d9f41d99de1681b5564d271a0b0
Abhishek Desikan <abhi.desikan@gmail.com>
2018-10-05 19:17:15 -0400

    Move zeroes updated

--- src/main/java/com/abhidesikan/interviewprep/leetcode/MoveZeroes.java
+++ src/main/java/com/abhidesikan/interviewprep/leetcode/MoveZeroes.java
@@ -4,25 +4,21 @@ import java.util.Arrays;
 
 public class MoveZeroes {
 
-	public static int[] moveZeroes(int[] nums) {
+	public void moveZeroes(int[] nums) {
 
-		int i = 0;
-		while (i < nums.length && nums[i] != 0) {
-			i++;
-		}
+		int j=0;
 
-		for (int j = i; j < nums.length; ) {
-			if (nums[i] != 0) {
-				i++;
-			} else {
-				if (nums[j] != 0) {
-					nums[i] = nums[j];
-					nums[j] = 0;
-				}
+		for(int i=0; i<nums.length; i++) {
+			if(nums[i] != 0) {
+				nums[j] = nums[i];
 				j++;
 			}
 		}
-		return nums;
+
+		while(j < nums.length) {
+			nums[j] = 0;
+			j++;
+		}
 	}
 
 	public static void main(String[] args) {
